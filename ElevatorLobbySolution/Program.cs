0ab52cfffa829263f7da6c21640c99357aee9fd3
David Hart <david.j.hart75@outlook.com>
2022-02-12 23:24:06 +0000

    Add project files.

---
+++ ElevatorLobbySolution/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorLobbySolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // So we need to create an elevator application.
+            // This will be built to be freely configurable and extensible
+            // from a design viewpoint, probably best to go from outside-in approach and use DI.
+
+            // So we want to centre around the configuration loading then the UI.
+            // foreach elevator in the collection create a form or UserControl, and give it it's own controller.
+            // the interface of our controllers can be implemented by many controller types;
+            // We may want an A&E service style - strict ordering 
+            // Or an eco-lift which reorders its requests to the nearest floors
+            // Or residential style - reordering new calls during moving is done in its current direction, calls from passed floors are added on return to the 'end'
+        }
+    }
+}
